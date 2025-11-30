2db8899c1105b28b8d956e6aea5754f8ae6b11fd
Tobias-Basilisco <tobiasbasilisco@gmail.com>
2025-11-30 13:37:13 +0100

    longestSong()

--- 103-stream-utilities-for-music-model/src/main/java/it/unibo/oop/lab/streams/MusicGroupImpl.java
+++ 103-stream-utilities-for-music-model/src/main/java/it/unibo/oop/lab/streams/MusicGroupImpl.java
@@ -1,5 +1,6 @@
 package it.unibo.oop.lab.streams;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -81,7 +82,9 @@ public final class MusicGroupImpl implements MusicGroup {
 
     @Override
     public Optional<String> longestSong() {
-        return Optional.empty();
+        return songs.stream()
+                    .max(Comparator.comparingDouble(Song::getDuration))
+                    .map(x -> x.getSongName());
     }
 
     @Override
